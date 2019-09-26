8db6ca1738c1a2ff64b6424970494f57906aa136
Mvr <eskander0001@gmail.com>
2019-09-26 13:17:16 +0300

    add the cart front end

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+
+                        @php
+                            $cart = 'cart_'.uniqid();
+                            $cart=[];
+                            \Session::put(['cart' =>$cart]);
+                        @endphp
+
+
+                        You are logged in!
+
+
+                        <div>The session ID is : {{\Session::getId()}}</div>
+
+                        <div class="text-danger">php uniqid : {{uniqid()}}</div>
+                        <div class="bg-danger">
+                         <strong>The session Cart is :   </strong>{{var_dump(\Session::get('cart'))}}
                         </div>
-                    @endif
 
-                    You are logged in!
+                        <div>
+                          <strong>uuid:</strong> {{\Str::uuid()}}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,58 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home
-                        <span class="sr-only">(current)</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
-<!-- Page Content -->
-<div class="container">
-
+@extends('layouts.master')
+@section('title')
+    AYMAN TITLE
+@stop
+@section('content')
     <!-- Jumbotron Header -->
     <header class="jumbotron my-4">
         @php
@@ -62,12 +12,15 @@
             $cartName=[];
             $cartName['A']='CCCCC';// way 1
             //$cartName=['B' =>'DDD'];// waY 2 - overwrite the WHOLE array !!
-            Session::put($cartName); // this must be associated arrray . Dont forget that the SESSION is assosiated array at the end of the day!
+            //Session::put($cartName); // this must be associated arrray . Dont forget that the SESSION is assosiated array at the end of the day!
+
+
+
 
             //dd($cartName);
         @endphp
         <h2 class="text-muted text-danger text-break">session::getId() {{Session::getId()}}</h2>
-        <h2 class="text-muted text-success text-break">cart: {{ Session::get($cartName)}}</h2>
+        <h2 class="text-muted text-success text-break">{{--cart: {{ Session::get($cartName)}}--}}</h2>
 
         <p class="lead">{{Session::get('Id','Defaullt Variable')}}
 
@@ -137,17 +90,14 @@
     </div>
     <!-- /.row -->
 
-</div>
-<!-- /.container -->
 
-<!-- Footer -->
-<footer class="py-5 bg-dark">
-    <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
-    </div>
-    <!-- /.container -->
-</footer>
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+        </div>
+        <!-- /.container -->
+    </footer>
 
 
-</body>
-</html>
+@stop
