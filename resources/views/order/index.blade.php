6935edb8b8b110e4bc332a808e2af6572b360624
Mvr <eskander0001@gmail.com>
2019-09-30 18:52:46 +0300

    add the ordre deserialize

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -11,60 +11,62 @@
 
 
         @foreach($orders as $order)
-            <div class="card-body">
-                # {{$loop->iteration}}
-                <ul class="list-group">
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        Amount
+            <li class="list-group-item d-flex justify-content-between align-items-center border-0">
+                <div class="lead"># {{$loop->iteration}}</div>
+            </li>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach($order->cart as $cartitem)
 
-                        <span class="badge badge-danger "> {{$order->amount}} $</span>
-                    </li>
-
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        invoice link <a href="{{$order->receipt_url}} ">Pdf</a>
-
-                    </li>
-
-
-                </ul>
-            </div>
-
-
-
-            <div class="card-body">
-                <ul class="list-group">
-                    @foreach($order->cart as $cartitem)
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            Brand
-                            <span class="badge badge-danger ">
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                    Brand
+                                    <span class="badge badge-danger ">
                             {{$cartitem['product']['brand']}}
                     </span>
 
-                        </li>
+                                </li>
 
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            Name
-                            <span class="badge badge-danger ">
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                    Name
+                                    <span class="badge badge-danger ">
                             {{$cartitem['product']['name']}}
                     </span>
 
-                        </li>
+                                </li>
 
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            Quantity
-                            <span class="badge badge-danger ">
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                    Quantity
+                                    <span class="badge badge-danger ">
                         {{$cartitem['quantity']}}
                          </span>
-                        </li>
+                                </li>
 
 
 
-                    @endforeach
-                </ul>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                Amount
+
+                                <span class="badge badge-danger "> {{$order->amount}} $</span>
+                            </li>
+
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                invoice link <a href="{{$order->receipt_url}} ">Pdf</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
-
-
-
         @endforeach
 
 
