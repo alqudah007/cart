55f797b48b1ad1949ad7cc556f0dc36ab8d503bb
Mvr <eskander0001@gmail.com>
2019-10-20 17:09:20 +0300

    add the contnet to the soultion

--- database/migrations/2019_10_16_143232_create_ayman_table.php
+++ database/migrations/2019_10_16_143232_create_ayman_table.php
@@ -11,7 +11,7 @@ class CreateAymanTable extends Migration
      *
      * @return void
      */
-    public function ~up()
+    public function up()
     {
         Schema::create('aymans', function (Blueprint $table) {
             $table->bigIncrements('id');
